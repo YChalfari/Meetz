f8187db8a7b1394938fdf0fd37b1fe2a6bb503a0
Yitz <chalfari@gmail.com>
2022-02-03 17:10:30 +0200

    pushed from wrong dir

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,35 @@
 const app = require("./app");
+const http = require("http")
+const cors = require("cors")
+const server = http.createServer(app)
+const io = require("socket.io")(server,{
+  cors: {
+    origin:"http://localhost:3000",
+    methods: ["GET","POST"  ]
+  }
+})
 require("dotenv").config();
 const port = process.env.PORT;
-app.listen(port, () => {
+
+//sockets
+io.on("connection", (socket)=>{
+  socket.emit("me", socket.id)
+  console.log("connected");
+  socket.on("disconnect", ()=>{
+    socket.broadcast.emit("callEnded")
+  })
+  //call user
+  socket.on("callUser", (data)=>{
+    io.to(data.userToCall).emit("callUser", {signal:data.signalData, from: data.from, name:data.name})
+  })
+  //answer call
+  socket.on("answerCall", (data)=>{
+    io.to(data.to).emit("callAccepted", data.signal)
+  })
+})
+
+
+
+server.listen(port, () => {
   console.log("Server is up on port " + port);
 });
